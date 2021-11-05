ea98c659e94e86e3ed4c9cc1138a4464dc997f29
vamsikrish-A <krishnavamsi165@gmail.com>
2021-11-05 23:55:36 +0530

    Added: given Multiple Raids Should Return total multiple fares

--- src/test/java/invoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/invoicegenerator/InvoiceGeneratorTest.java
@@ -33,4 +33,14 @@ public class InvoiceGeneratorTest {
         Assertions.assertEquals(0.0, fare,0.0);
 
     }
+    @Test
+    public void givenMultipleRides_ShouldReturnMultipleRaidsTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30, fare,0.0);
+
+    }
 }
