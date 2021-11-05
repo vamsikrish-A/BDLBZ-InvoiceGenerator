17437cb33ba23ed8ae4d11aee25f160b2a919eac
vamsikrish-A <krishnavamsi165@gmail.com>
2021-11-06 00:06:42 +0530

    Refactor: invoice generator for invoice summary for multiple rides with fare

--- src/main/java/invoicegenerator/InvoiceSummary.java
+++ src/main/java/invoicegenerator/InvoiceSummary.java
@@ -1,7 +1,21 @@
 package invoicegenerator;
 
 public class InvoiceSummary {
-    public InvoiceSummary(int i, double v) {
+    private final double numberOfRides;
+    private final double totalFare;
+    private final double averageFare;
 
+    public InvoiceSummary(int fare, double rides) {
+        this.numberOfRides = rides;
+        this.totalFare = fare;
+        this.averageFare = this.totalFare/ this.numberOfRides;
+
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary summary = (InvoiceSummary) o;
+        return Double.compare(summary.numberOfRides, numberOfRides) == 0 && totalFare == summary.totalFare && Double.compare(summary.averageFare, averageFare) == 0;
     }
 }
