7e7e2102ab8ed3c6218a72e8a0fa250aca520671
vamsikrish-A <krishnavamsi165@gmail.com>
2021-11-05 23:41:58 +0530

    Refactored: Code to caluculate fare for given distance and time return equals

--- src/main/java/invoicegenerator/InvoiceGenerator.java
+++ src/main/java/invoicegenerator/InvoiceGenerator.java
@@ -1,7 +1,23 @@
 package invoicegenerator;
+/*
+* @purpose: Cab Invoice Generator, where the customer books the cab pays at end of month
+* @since: 05-nov
+* @author: Vamsi Krishna
+* */
 
 public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KM = 10.0;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5.0;
+
+    /*
+    * Method calculating Fare for the given distance and time
+    * @params: given distance and time, costPerKm, CostPerTime
+    *  */
     public double calculateFare(double distance, int time) {
-        return 0;
+        double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
+        if (totalFare < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalFare;
     }
 }
